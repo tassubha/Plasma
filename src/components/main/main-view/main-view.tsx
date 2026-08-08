a88a2416acae666907e5b7f45fbda8e4099e2243
Tasniah Shova <tass.subha@gmail.com>
2026-08-08 21:31:10 +0600

    fixed main view height issue

--- src/components/main/main-view/main-view.tsx
+++ src/components/main/main-view/main-view.tsx
@@ -14,17 +14,17 @@ function MainView({
     tabState: ReactState<TabPage>
 }) {
     return (
-        <>
+        <div className="flex flex-col h-screen">
             <h1 className="font-serif font-bold text-4xl py-2 px-4">{tabState.value}</h1>
             <HorizontalRuler className="bg-stone-400"/>
-            <div className="w-full h-full p-4 overflow-auto relative">
+            <div className="flex-1 p-4 overflow-auto relative">
                 <PageSwitch
                     user={user}
                     userData={userData}
                     tabState={tabState}
                 />
             </div>
-        </>
+        </div>
     );
 }
 
